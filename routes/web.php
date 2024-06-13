04b62f80da58b3ca8203840f7c80e8d53aa90a6d
jengis <jengis.d@gmail.com>
2024-06-14 02:53:51 +0600

    Add some routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::permanentRedirect('/home', '/');
+Route::view('/about', 'about');
