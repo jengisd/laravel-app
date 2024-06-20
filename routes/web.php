9931687286c8e5bc25d5a916c3a1a5e020119d6a
jengis <jengis.d@gmail.com>
2024-06-20 20:41:18 +0600

    Add routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,3 +10,13 @@ Route::get('/', function () {
 
 Route::permanentRedirect('/home', '/');
 Route::view('/about', 'about');
+
+Route::get('/users', function () {
+    $users = DB::select('select * from users');
+    return view('users' , ['users' => $users]);
+});
+
+Route::get('/users/{id}', function ($id) {
+    $user = DB::selectOne('select * from users where id = ?', [$id]);
+    return view('user' , ['user' => $user]);
+});
